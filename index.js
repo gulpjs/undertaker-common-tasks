65d5ebd68e156a15da379a7230dd3c9320836fbc
Blaine Bublitz <blaine@iceddev.com>
2014-09-13 17:17:50 -0700

    initial tasks

---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var fs = require('fs');
+var http = require('http');
+var util = require('util');
+
+var del = require('del');
+var gutil = require('gulp-util');
+var ecstatic = require('ecstatic');
+var DefaultRegistry = require('undertaker-registry');
+
+function CommonRegistry(){
+  DefaultRegistry.call(this);
+
+  var port = 8080;
+  var buildDir = './build';
+  var exists = fs.existsSync(buildDir);
+
+  if(exists){
+    throw new Error('Cannot initialize undertaker-common-tasks registry. `build/` directory exists.');
+  }
+
+  this.set('clean', function(cb){
+    del([buildDir], cb);
+  });
+
+  this.set('serve', function(cb){
+    http.createServer(
+      ecstatic({ root: buildDir })
+    ).listen(port, function(){
+      gutil.log('Server started at http://0.0.0.0:' + port);
+      cb();
+    });
+  });
+}
+
+util.inherits(CommonRegistry, DefaultRegistry);
