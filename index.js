f606c3940a718fb94cc80536561d7ce091e4a7f6
Peter deHaan <peter@deseloper.com>
2015-10-25 17:16:47 -0700

    Add ESLint and JSCS

--- index.js
+++ index.js
@@ -9,36 +9,36 @@ var logger = require('gulplog');
 var ecstatic = require('ecstatic');
 var DefaultRegistry = require('undertaker-registry');
 
-function CommonRegistry(opts){
+function CommonRegistry(opts) {
   DefaultRegistry.call(this);
 
   opts = opts || {};
 
   this.config = {
     port: opts.port || 8080,
-    buildDir: opts.buildDir || './build'
+    buildDir: opts.buildDir || './build',
   };
 }
 
 util.inherits(CommonRegistry, DefaultRegistry);
 
-CommonRegistry.prototype.init = function init(taker){
+CommonRegistry.prototype.init = function init(taker) {
   var port = this.config.port;
   var buildDir = this.config.buildDir;
   var exists = fs.existsSync(buildDir);
 
-  if(exists){
+  if (exists) {
     throw new Error('Cannot initialize undertaker-common-tasks registry. `build/` directory exists.');
   }
 
-  taker.task('clean', function(){
+  taker.task('clean', function() {
     return del([buildDir]);
   });
 
-  taker.task('serve', function(cb){
+  taker.task('serve', function(cb) {
     http.createServer(
       ecstatic({ root: buildDir })
-    ).listen(port, function(){
+    ).listen(port, function() {
       logger.info('Server started at http://0.0.0.0:' + port);
       cb();
     });
