171aafa1ffc9aad60077f320f349dde466effbc3
sttk <sttk.xslet@gmail.com>
2022-06-19 16:57:58 +0900

    fix: modify dir path in existence check error

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ CommonRegistry.prototype.init = function init(taker) {
   var exists = fs.existsSync(buildDir);
 
   if (exists) {
-    throw new Error('Cannot initialize undertaker-common-tasks registry. '
-      + '`build/` directory exists.');
+    throw new Error('Cannot initialize undertaker-common-tasks registry. `' +
+      buildDir + '` directory exists.');
   }
 
   taker.task('clean', function() {
